5d75f5bc265627071ae53760f369187ee479ff71
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-17 10:26:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,6 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  x = 0
   holiday_hash.each do |season, hash|
     puts "#{season.capitalize}:"
     hash.each do |holiday, array|
