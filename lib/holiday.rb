52f7bfe18081b59cf488f347b60d40666d708043
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-17 10:06:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,8 +50,7 @@ def all_supplies_in_holidays(holiday_hash)
       end 
       puts "  #{holiday.capitalize}: #{array.join(", "}"
       end
-
-  end 
+    end
 end
 
 def all_holidays_with_bbq(holiday_hash)
